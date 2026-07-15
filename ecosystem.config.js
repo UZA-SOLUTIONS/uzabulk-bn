9f4ff294deaf41730c8b454f84c6b91c12188e84
Kgad <kalisagad05@gmail.com>
2026-07-15 23:14:50 +0800

    Resolve locales/en.json conflict after pull

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
             name: "uza-customer-api",
             script: "./server.js",
             watch: false,
+            max_memory_restart: "500M",
+            kill_timeout: 5000,
             env_staging: {
                 "PORT": process.env.PORT || 1302,
                 "NODE_ENV": "development"
